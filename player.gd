07c2f9717c2b5ed6a5add4bac47c971fec9776f8
Artem <serdyukartyom.zp@gmail.com>
2026-05-04 08:11:03 +0300

    add win and crash sygnals

--- player.gd
+++ player.gd
@@ -4,11 +4,19 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_accept"):
+	if Input.is_action_pressed("boost"):
 		apply_central_force(basis.y * delta * 1000)
 		
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("left"):
 		apply_torque(Vector3(0.0, 0.0, 100.0 * delta))
 		
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("right"):
 		apply_torque(Vector3(0.0, 0.0, -100.0 * delta))
+
+
+func _on_body_entered(body: Node) -> void:
+	if "Goal" in body.get_groups():
+		print("Win")
+	if "Floor" in body.get_groups():
+		print("Lose")
+	
