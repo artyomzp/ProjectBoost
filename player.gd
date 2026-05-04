d9363442dd385e5c60a50e94b8d16c064aec0493
Artem <serdyukartyom.zp@gmail.com>
2026-05-04 18:50:48 +0300

    add crash and win sequences

--- player.gd
+++ player.gd
@@ -1,22 +1,38 @@
 extends RigidBody3D
 
+## How much vertical force to apply when moving
+@export_range(750.0, 3000.0) var trust: float = 1000.0
+
+## How much torque force to apply when moving
+@export var torque_trust: float = 100.0
+
 func _ready() -> void:
 	pass # Replace with function body.
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
-		apply_central_force(basis.y * delta * 1000)
+		apply_central_force(basis.y * delta * trust)
 		
 	if Input.is_action_pressed("left"):
-		apply_torque(Vector3(0.0, 0.0, 100.0 * delta))
+		apply_torque(Vector3(0.0, 0.0, torque_trust * delta))
 		
 	if Input.is_action_pressed("right"):
-		apply_torque(Vector3(0.0, 0.0, -100.0 * delta))
+		apply_torque(Vector3(0.0, 0.0, -torque_trust * delta))
 
 
 func _on_body_entered(body: Node) -> void:
 	if "Goal" in body.get_groups():
-		print("Win")
-	if "Floor" in body.get_groups():
-		print("Lose")
+		complete_level()
+	if "Hazard" in body.get_groups():
+		crash_sequence()
+		
+func crash_sequence() -> void:
+	print("Lose")
+	get_tree().reload_current_scene()
+	
+func complete_level() -> void:
+	print("Win")
+	get_tree().quit()
+	
+	
 	
