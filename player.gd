ba30b8fcbbeb3f1562c87b8c6c66dfcd69250780
Artem <serdyukartyom.zp@gmail.com>
2026-05-05 08:09:21 +0300

    add sounds effect

--- player.gd
+++ player.gd
@@ -6,6 +6,10 @@ extends RigidBody3D
 ## How much torque force to apply when moving
 @export var torque_trust: float = 100.0
 
+@onready var explosion_audio: AudioStreamPlayer = $ExplosionAudio
+@onready var success_audio: AudioStreamPlayer = $SuccessAudio
+@onready var rocket_engine: AudioStreamPlayer3D = $RocketEngine
+
 var is_transitioning: bool = false
 
 func _ready() -> void:
@@ -13,7 +17,11 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
+		if rocket_engine.playing == false:
+			rocket_engine.play()
 		apply_central_force(basis.y * delta * trust)
+	else:
+		rocket_engine.stop()
 		
 	if Input.is_action_pressed("left"):
 		apply_torque(Vector3(0.0, 0.0, torque_trust * delta))
@@ -32,18 +40,20 @@ func _on_body_entered(body: Node) -> void:
 		
 func crash_sequence() -> void:
 	print("Lose")
+	explosion_audio.play()
 	set_process(false)
 	is_transitioning = true
 	var tween = create_tween()
-	tween.tween_interval(1.0)
+	tween.tween_interval(2.5)
 	tween.tween_callback(get_tree().reload_current_scene)
 	
 func complete_level(next_level_file: String) -> void:
 	print("Win")
+	success_audio.play()
 	set_process(false)
 	is_transitioning = true
 	var tween = create_tween()
-	tween.tween_interval(1.0)
+	tween.tween_interval(2.5)
 	tween.tween_callback(
 		get_tree().change_scene_to_file.bind(next_level_file)
 	)
